29d88adacb74d6ac75036cc6821f86b4fbe34edf
williamrr <william.rodriguez@alteru.co>
2021-07-14 18:01:04 -0500

    faltan unos detalles proyecto al 90%

---
+++ src/lazy.js
@@ -0,0 +1,19 @@
+// lazy-loadin
+const isIntersecting = (entry) => {
+    return entry.isIntersecting;
+};
+
+const accion = (entry) => {
+    const container = entry.target;
+    const imagen = container.firstChild;
+    const url = imagen.dataset.src
+    imagen.src = url
+    observer.unobserve(container);
+};
+const observer = new IntersectionObserver((entries) => {
+    entries.filter(isIntersecting).forEach(accion);
+});
+
+export const registerImage = (imagen) => {
+    observer.observe(imagen)
+}
